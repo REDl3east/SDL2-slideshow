ba9f22563f3448df645d62fa978101a9f38857c6
Dalton Overmyer <dalton@raptorpowersystems.com>
2021-04-27 22:26:48 -0500

    added fit_image and started inital impl

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,31 @@ std::vector<std::string> get_filenames_from_path(const std::string &path) {
   return out;
 }
 
+SDL_Rect fit_image(SDL_Window *window, SDL_Surface *surface) {
+  SDL_Rect out;
+  int window_w, window_h;
+  SDL_GetWindowSize(window, &window_w, &window_h);
+
+  out.x = 0;
+  out.y = 0;
+  out.w = surface->w;
+  out.h = surface->h;
+
+  if(window_h > surface->h){
+    
+  }else{
+
+  }
+
+  if(window_w > surface->w){
+
+  }else{
+
+  }
+
+  return out;
+}
+
 int main(int argc, char **argv) {
   //obtain file names from assets
   auto filenames = get_filenames_from_path("assets");
@@ -32,24 +57,35 @@ int main(int argc, char **argv) {
   SDL_Init(SDL_INIT_EVERYTHING);
   IMG_Init(IMG_INIT_PNG);
 
-  SDL_Window *window = SDL_CreateWindow(APP_NAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE & 0);
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+  SDL_Window *window = SDL_CreateWindow(APP_NAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 
-  auto *current_image = IMG_Load(filenames[0].c_str());
+  SDL_Surface *current_image = IMG_Load(filenames[7].c_str());
   SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, current_image);
+
+  int duration = 5000;
 
   SDL_Event event;
   bool quit = false;
   while (!quit) {
-    SDL_WaitEvent(&event);
-
-    switch (event.type) {
-      case SDL_QUIT:
-        quit = true;
-        break;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_QUIT:
+          quit = true;
+          break;
+      }
     }
 
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    if (quit) break;
+
+    int window_w, window_h;
+    SDL_GetWindowSize(window, &window_w, &window_h);
+    // std::cout << "W: " << window_w << "H: " << window_h << '\n';
+
+    // SDL_Rect r = fit_image(window, current_image);
+    SDL_Rect r = {0, 0, 480, 360};
+
+    SDL_RenderCopy(renderer, texture, NULL, &r);
     SDL_RenderPresent(renderer);
   }
 
