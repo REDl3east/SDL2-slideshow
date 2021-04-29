0c560efd6609d11ce37d25e0eeaf6fff8a2fd2c7
Dalton Overmyer <dalton@raptorpowersystems.com>
2021-04-29 02:26:18 -0500

    fit_image impl complete

--- src/main.cpp
+++ src/main.cpp
@@ -21,33 +21,31 @@ std::vector<std::string> get_filenames_from_path(const std::string &path) {
   return out;
 }
 
-void fit_image(SDL_Rect& window_rect, SDL_Rect& surface_rect, SDL_Rect& out_rect) {
-  out_rect.x = 0;
-  out_rect.y = 0;
-  out_rect.w = surface_rect.w;
-  out_rect.h = surface_rect.h;
-
-  if (window_rect.h > surface_rect.h && window_rect.w > surface_rect.w) {
+void fit_image(SDL_Rect &window_rect, SDL_Rect &surface_rect, SDL_Rect &out_rect) {
+  if (window_rect.h >= surface_rect.h && window_rect.w >= surface_rect.w) {
     out_rect.w = surface_rect.w;
     out_rect.h = surface_rect.h;
     out_rect.x = (window_rect.w - surface_rect.w) / 2;
     out_rect.y = (window_rect.h - out_rect.h) / 2;
-  } else if (window_rect.h > surface_rect.h && window_rect.w < surface_rect.w) {
+  } else if (window_rect.h >= surface_rect.h && window_rect.w <= surface_rect.w) {
     out_rect.w = window_rect.w;
     out_rect.h = ((double)window_rect.w / (double)surface_rect.w) * surface_rect.h;
     out_rect.x = 0;
     out_rect.y = (window_rect.h - out_rect.h) / 2;
-  } else if (window_rect.h < surface_rect.h && window_rect.w > surface_rect.w) {
+  } else if (window_rect.h <= surface_rect.h && window_rect.w >= surface_rect.w) {
     out_rect.w = ((double)window_rect.h / (double)surface_rect.h) * surface_rect.w;
     out_rect.h = window_rect.h;
     out_rect.x = (window_rect.w - out_rect.w) / 2;
     out_rect.y = 0;
-  } else if (window_rect.h < surface_rect.h && window_rect.w < surface_rect.w) {
+  } else if (window_rect.h <= surface_rect.h && window_rect.w <= surface_rect.w) {
     //calculate the new surface
+    surface_rect.w = ((double)window_rect.h / (double)surface_rect.h) * surface_rect.w;
+    surface_rect.h = window_rect.h;
+    fit_image(window_rect, surface_rect, out_rect);
   }
 }
 
-void fit_image(SDL_Window *window, SDL_Surface *surface, SDL_Rect& rect_out) {
+void fit_image(SDL_Window *window, SDL_Surface *surface, SDL_Rect &rect_out) {
   int window_w, window_h;
   SDL_GetWindowSize(window, &window_w, &window_h);
 
@@ -56,8 +54,6 @@ void fit_image(SDL_Window *window, SDL_Surface *surface, SDL_Rect& rect_out) {
 
   fit_image(window_rect, surface_rect, rect_out);
 }
-
-
 
 int main(int argc, char **argv) {
   //obtain file names from assets
@@ -72,13 +68,10 @@ int main(int argc, char **argv) {
 
   SDL_Window *window = SDL_CreateWindow(APP_NAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-
-  SDL_Surface *current_image = IMG_Load(filenames[7].c_str());
+  SDL_Surface *current_image = IMG_Load(filenames[5].c_str());
   SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, current_image);
 
-  int duration = 5000;
-
-  int i = 0;
+  SDL_SetWindowMinimumSize(window, 250, 250);
 
   SDL_Event event;
   bool quit = false;
@@ -98,9 +91,8 @@ int main(int argc, char **argv) {
 
     SDL_Rect r;
     fit_image(window, current_image, r);
-    // SDL_Rect r = {0, 0, 720, 360};
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, texture, NULL, &r);
     SDL_RenderPresent(renderer);
